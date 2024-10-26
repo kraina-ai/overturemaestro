a8f90a9ae68c32cf499b5e3a137274ce0e00aac2
Kamil Raczycki <kamil.raczycki@allegro.com>
2024-10-26 23:26:49 +0200

    feat: move geocoding functioin to main api

--- overturemaestro/__init__.py
+++ overturemaestro/__init__.py
@@ -15,6 +15,7 @@ from overturemaestro.functions import (
     convert_geometry_to_parquet,
     convert_geometry_to_parquet_for_multiple_types,
 )
+from overturemaestro.geocode import geocode_to_geometry
 from overturemaestro.release_index import (
     get_available_release_versions,
     get_available_theme_type_pairs,
@@ -33,6 +34,7 @@ __all__ = [
     "convert_geometry_to_geodataframe_for_multiple_types",
     "convert_geometry_to_parquet",
     "convert_geometry_to_parquet_for_multiple_types",
+    "geocode_to_geometry",
     "get_available_release_versions",
     "get_available_theme_type_pairs",
     "get_newest_release_version",
