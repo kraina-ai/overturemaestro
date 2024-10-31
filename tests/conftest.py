a02fcb67feb4de12c3b7d2d58f0eb546c2d18003
Kamil Raczycki <kamil.raczycki@allegro.com>
2024-10-31 23:09:40 +0100

    chore: add github dependency mocking

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 import pytest
 from pytest import Item
+from pytest_mock import MockerFixture
 from shapely import Polygon, box, to_geojson, to_wkt
 
 from overturemaestro.release_index import download_existing_release_index
@@ -27,6 +28,24 @@ def test_release_version() -> str:
 def download_release_index(test_release_version: str) -> None:
     """Download release index for testing purposes."""
     download_existing_release_index(release=test_release_version)
+
+
+@pytest.fixture(autouse=True, scope="session")  # type: ignore
+def patch_get_available_versions(session_mocker: MockerFixture) -> None:
+    """Mock getting available release versions without GitHub."""
+    session_mocker.patch(
+        "overturemaestro.release_index._load_all_available_release_versions_from_github",
+        return_value=[
+            "2024-04-16-beta.0",
+            "2024-05-16-beta.0",
+            "2024-06-13-beta.0",
+            "2024-06-13-beta.1",
+            "2024-07-22.0",
+            "2024-08-20.0",
+            "2024-09-18.0",
+            "2024-10-23.0",
+        ],
+    )
 
 
 def bbox() -> tuple[float, float, float, float]:
