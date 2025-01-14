79ffc8d437eb746455e1af4cd18ba7d72e409561
Kamil Raczycki <raczyckikamil@gmail.com>
2025-01-14 13:02:47 +0100

    feat: finish wide form tests

--- tests/base/advanced/test_wide_form.py
+++ tests/base/advanced/test_wide_form.py
@@ -1,6 +1,7 @@
 """Tests for public API advanced wide form functions."""
 
 from contextlib import nullcontext as does_not_raise
+from pathlib import Path
 from typing import Any, Optional
 
 import pytest
@@ -18,19 +19,35 @@ from overturemaestro.data_downloader import PYARROW_FILTER
 from tests.conftest import bbox
 
 
+@pytest.fixture(scope="session")  # type: ignore
+def wide_form_working_directory() -> Path:
+    """Working directory for tests."""
+    return Path("files/wide_form")
+
+
+@pytest.fixture(autouse=True, scope="session")  # type: ignore
+def clear_working_directory(wide_form_working_directory: Path) -> None:
+    """Clear working directory."""
+    for file_path in wide_form_working_directory.glob("**/*.parquet"):
+        file_path.unlink()
+
+
 @pytest.mark.parametrize(
     "theme_type_pair",
     list(THEME_TYPE_CLASSIFICATION.keys()),
 )  # type: ignore
-def test_theme_type_pairs(theme_type_pair: tuple[str, str], test_release_version: str) -> None:
+def test_theme_type_pairs(
+    test_release_version: str, wide_form_working_directory: Path, theme_type_pair: tuple[str, str]
+) -> None:
     """Test if all theme type pairs are working."""
     convert_bounding_box_to_wide_form_parquet(
         theme=theme_type_pair[0],
         type=theme_type_pair[1],
         bbox=bbox(),
         release=test_release_version,
+        working_directory=wide_form_working_directory,
         verbosity_mode="verbose",
-        ignore_cache=True,
+        ignore_cache=False,
     )
 
 
@@ -38,14 +55,17 @@ def test_theme_type_pairs(theme_type_pair: tuple[str, str], test_release_version
     "hierarchy_value",
     [None, 1],
 )  # type: ignore
-def test_all_theme_type_pairs(hierarchy_value: Optional[int], test_release_version: str) -> None:
+def test_all_theme_type_pairs(
+    test_release_version: str, wide_form_working_directory: Path, hierarchy_value: Optional[int]
+) -> None:
     """Test if downloading all theme type pairs at once is working."""
     gdf = convert_bounding_box_to_wide_form_geodataframe_for_all_types(
         bbox=bbox(),
         release=test_release_version,
+        working_directory=wide_form_working_directory,
         hierarchy_depth=hierarchy_value,
         verbosity_mode="verbose",
-        ignore_cache=True,
+        ignore_cache=False,
     )
     prepared_theme_type_prefixes = tuple("|".join(t) for t in THEME_TYPE_CLASSIFICATION.keys())
     feature_columns = [
@@ -116,6 +136,7 @@ def test_all_theme_type_pairs(hierarchy_value: Optional[int], test_release_versi
 )  # type: ignore
 def test_multiple_theme_type_pairs(
     test_release_version: str,
+    wide_form_working_directory: Path,
     theme_type_pairs: list[tuple[str, str]],
     pyarrow_filters: Optional[list[Optional[PYARROW_FILTER]]],
     hierarchy_value: Optional[int],
@@ -127,10 +148,11 @@ def test_multiple_theme_type_pairs(
             theme_type_pairs=theme_type_pairs,
             bbox=bbox(),
             release=test_release_version,
+            working_directory=wide_form_working_directory,
             hierarchy_depth=hierarchy_value,
             pyarrow_filters=pyarrow_filters,
             verbosity_mode="verbose",
-            ignore_cache=True,
+            ignore_cache=False,
         )
         prepared_theme_type_prefixes = tuple("|".join(t) for t in theme_type_pairs)
         feature_columns = [
@@ -150,6 +172,7 @@ def test_multiple_theme_type_pairs(
 @P.case("Third value", 3, ("base", "water"), pytest.raises(HierarchyDepthOutOfBoundsError))  # type: ignore
 def test_hierarchy_values(
     test_release_version: str,
+    wide_form_working_directory: Path,
     hierarchy_value: Optional[int],
     theme_type_pair: tuple[str, str],
     expectation: Any,
@@ -162,8 +185,9 @@ def test_hierarchy_values(
             bbox=bbox(),
             hierarchy_depth=hierarchy_value,
             release=test_release_version,
+            working_directory=wide_form_working_directory,
             verbosity_mode="verbose",
-            ignore_cache=True,
+            ignore_cache=False,
         )
         assert "geometry" in gdf.columns
         feature_columns = [
