5d44a2c99a9ee75fd360daec36ac64d97dc2b593
Kamil Raczycki <raczyckikamil@gmail.com>
2026-04-18 22:32:26 +0200

    fix: check if result parquet file has not geo metadata - load it via wkb parsing

--- overturemaestro/geopandas_io.py
+++ overturemaestro/geopandas_io.py
@@ -3,9 +3,10 @@
 from pathlib import Path
 
 import geopandas as gpd
+import pandas as pd
 import pyarrow.parquet as pq
 
-from overturemaestro._constants import INDEX_COLUMN
+from overturemaestro._constants import GEOMETRY_COLUMN, INDEX_COLUMN
 
 
 def geodataframe_from_parquet(parquet_path: Path) -> gpd.GeoDataFrame:
@@ -15,7 +16,12 @@ def geodataframe_from_parquet(parquet_path: Path) -> gpd.GeoDataFrame:
     Loads the file without partitioning columns. Creates an index based on the id column.
     """
     schema = pq.ParquetDataset(parquet_path, partitioning=None).schema
-    gdf = gpd.read_parquet(parquet_path, partitioning=None, columns=schema.names)
+    try:
+        gdf = gpd.read_parquet(parquet_path, partitioning=None, columns=schema.names)
+    except ValueError:
+        df = pd.read_parquet(parquet_path, partitioning=None, columns=schema.names)
+        gdf = gpd.GeoDataFrame(df, geometry=gpd.GeoSeries.from_wkb(df[GEOMETRY_COLUMN], crs=4326))
+
     if INDEX_COLUMN in gdf.columns:
         gdf.set_index(INDEX_COLUMN, inplace=True)
 
